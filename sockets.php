872b7e33f47a017563dd6aa57a91cc30eb1d4e3b
Eric Stern <firehed@gmail.com>
2010-12-05 02:42:34 -0800

    Rearrange executable for error reporting and debugging

--- sockets.php
+++ sockets.php
@@ -1,20 +1,18 @@
 #!/usr/bin/php
 <?php
 set_time_limit(0);
+error_reporting(-1);
+
 include './config.php';
+set_error_handler(function($no,$str,$f,$l) {
+	// Handle socket exceptions differently
+	if (strstr($str, 'socket_'))
+		throw new SocketException;
 
-if (DEBUG) {
-	error_reporting(-1);
-	set_error_handler(function($no,$str,$f,$l) {
-		// Handle socket exceptions differently
-		if (strstr($str, 'socket_'))
-			throw new SocketException;
-			
+	if (DEBUG)
 		print_r(func_get_args());
-		throw new ErrorException($str,0,$no,$f,$l);
-	}, -1);
-}
-
+	throw new ErrorException($str,0,$no,$f,$l);
+}, -1);
 
 include './app/Action.php';
 include './app/Client.php';
