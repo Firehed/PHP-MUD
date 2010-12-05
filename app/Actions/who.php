976d5b850c1db54f6ddcd3cdc3287f0ea8dcea98
Eric Stern <firehed@gmail.com>
2010-12-05 13:04:23 -0800

    Remove some cruft in "who" action

--- app/Actions/who.php
+++ app/Actions/who.php
@@ -5,11 +5,8 @@ class who implements Action {
 	} // function ok
 
 	public static function run(Client $client, $input) {
-		//$users = array();
 		foreach (Server::getClients() as $c) {
-			//$users[] = $c->user;
 			$client->message('[some info...] ' . $c->user->name);
 		}
-
 	} // function run
 }
