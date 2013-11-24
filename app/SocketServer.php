c3c54c395da4e8bb1e365ae2c2a693a5fd69f8e7
Eric Stern <eric@ericstern.com>
2013-11-24 13:02:34 -0800

    Overhaul of SocketServer
    Summary:
    Decouple initial socket_create from constructor
    Allow socket domain and type to be set (default AF_INET, SOCK_STREAM)
    Use domain and type to create socket on start
    If AF_UNIX is specified, ensure that closing the server cleans up the file
    Close on __destruct too (can't guarantee handling for fatal/SIGTERM)
    
    make timeout configurable
    
    Test Plan: ran, worked, etc.
    
    Differential Revision: https://phabricator.ericstern.com/D8

--- app/SocketServer.php
+++ app/SocketServer.php
@@ -11,35 +11,109 @@ class SocketServer implements SocketClientDelegate {
 	private $clients = [];     // SocketClient objects
 	private $connections = []; // Raw client resources (required for reading)
 	private $delegate;         // Delegate to receive info
+	private $domain = AF_INET; // Socket domain
+	private $id;
 	private $socket;           // Main server socket
+	private $started = false;  // Are we listening?
+	private $timeoutSec = 0;   // How long should socket wait before timeout?
+	private $timeoutUsec = 100000; // above, microsecond component
+	private $type = SOCK_STREAM; // Socket type
 
 	public function __construct(SocketServerDelegate $delegate) {
-		$socket = socket_create(AF_INET, SOCK_STREAM, 0);
-		socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
-		socket_set_nonblock($socket);
-		$this->socket = $socket;
 		$this->delegate = $delegate;
+		$this->id = spl_object_hash($this);
+	}
+
+	public function __destruct() {
+		$this->close();
+	}
+
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 * Set timeout of activity poll, in seconds
+	 * Defaults to 0.1
+	 * @param float $timeout
+	 * @return this
+	 */
+	public function setTimeout($timeout) {
+		if (!is_int($timeout) && !is_float($timeout)) {
+			throw new Exception(__METHOD__." must be passed a positive number");
+		}
+		if ($timeout <= 0) {
+			throw new Exception(__METHOD__." must be passed a positive number");
+		}
+		$this->timeoutSec = (int) $timeout; // interger part (implicit floor)
+		$this->timeoutUsec = $timeout * 1000000 % 1000000; // fractional part, µs
+		return $this;
+	}
+
+	// Todo: abstract these better
+	public function setDomain($domain) {
+		switch ($domain) {
+		case AF_INET:
+		case AF_INET6:
+		case AF_UNIX:
+			$this->domain = $domain;
+			return $this;
+		default:
+			throw new Exception("Domain must be one of AF_INET, AF_INET6, AF_UNIX");
+		}
+	}
+
+	// Todo: abstract these better
+	public function setType($type) {
+		switch ($type) {
+		case SOCK_STREAM:
+			$this->type = $type;
+			return $this;
+		case SOCK_DGRAM:
+		case SOCK_SEQPACKET:
+		case SOCK_RAW:
+		case SOCK_RDM:
+			throw new Exception("Not sure if that type is safe, rejecting");
+		default:
+			throw new Execption("Type must be one of SOCK_XXX constants");
+		}
 	}
 
 	public function start($address, $port) {
-		if (!socket_bind($this->socket, $address, $port)) {
-			throw new Exception("Can't bind to port $port.");
+		$this->address = $address;
+		$this->port = $port;
+		$socket = socket_create($this->domain, $this->type, 0);
+		socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
+		socket_set_nonblock($socket);
+
+		if (!socket_bind($socket, $address, $port)) {
+			throw new Exception("Can't bind to $address:$port.");
 		}
-		if (!socket_listen($this->socket)) {
+
+		if (!socket_listen($socket)) {
 			throw new Exception("Could not start listening");
 		}
+		$this->socket = $socket;
+		$this->started = true;
+		return $this;
 	}
 
 	public function close() {
+		if (!$this->started) {
+			return;
+		}
 		// Clean up all clients first?
-		Log::info("Closing main socket");
 		socket_close($this->socket);
+		if (AF_UNIX == $this->domain) {
+			unlink($this->address);
+		}
+		$this->started = false;
 	}
 
 	/**
 	 * @return resource or null
 	 */
-	public function accept() {
+	private function accept() {
 		if ($raw = socket_accept($this->socket)) {
 			return $raw;
 		}
@@ -58,7 +132,8 @@ class SocketServer implements SocketClientDelegate {
 		// Copy socket resouces for polling
 		$sockets = [$this->socket] + $this->connections;
 		// Look for changes
-		if (!socket_select($sockets, $write, $except, 0, 100000)) {
+		if (!socket_select($sockets, $write, $except, $this->timeoutSec,
+			$this->timeoutUsec)) {
 			// nothing to do!
 			return;
 		}
